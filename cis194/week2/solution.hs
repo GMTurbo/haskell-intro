ee6837feec156edc23ee496bce7dca210f163d22
GMTurbo <gabe.testa@gmail.com>
2015-04-28 12:39:05 -0600

    Update solution.hs
    not working

--- cis194/week2/solution.hs
+++ cis194/week2/solution.hs
@@ -18,9 +18,31 @@ parseMessage str =
 
 -- Exercise 2
 insert :: LogMessage -> MessageTree -> MessageTree
-insert logMsg1@(LogMessage _ tsNew _) (Node left logMsg2@(LogMessage _ tsOld _ ) right)
+insert logMsg1@(LogMessage _ tsNew _) msgTree@(Node left logMsg2@(LogMessage _ tsOld _ ) right)
   | tsNew > tsOld = Node left logMsg1 (insert logMsg2 right)
   | otherwise = Node (insert logMsg1 left) logMsg2 right
 
 insert logMsg Leaf =
-  MessageTree Leaf logMsg Leaf
+  Node Leaf logMsg Leaf
+
+--Exercise 3
+
+build :: [LogMessage] -> MessageTree
+build messages = case messages of
+  []          -> Node Leaf (Unknown "empty") Leaf
+  (lm1:lm2:_) -> insert lm2 (Node Leaf lm1 Leaf)
+
+--Exercise 4
+inOrder :: MessageTree -> [LogMessage]
+inOrder (Node left logMsg right) =
+   (inOrder left) ++ [logMsg] ++ (inOrder right)
+inOrder Leaf = []
+--Exercise 5
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong messages =
+   filter (filterFun 50) (inOrder (build messages)) --bug here
+
+filterFun :: Int -> LogMessage -> Bool
+filterFun min (LogMessage (Error msgSev) _ _)
+ | msgSev > min = True
+ | otherwise = False
