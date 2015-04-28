3f798ea7d29a1ade0d8c40e5760d4cf53a1a1b69
GMTurbo <gabe.testa@gmail.com>
2015-04-28 08:40:26 -0600

    Update IO.hs

--- IO.hs
+++ IO.hs
@@ -1,1 +1,14 @@
 
+---------- Overview ----------
+-- Compile executable program
+-- IO Actions
+-- Combine IO Actions
+-- IO Values
+-- Useful IO Functions
+
+---------- Hello World ----------
+main = putStrLn "Hello World"
+
+-- save as .hs
+-- in terminal 'ghc HelloWorld.hs'
+-- now you have a .exe!
