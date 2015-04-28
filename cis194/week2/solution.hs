4b40f34bbb6bb02e3a16517650ac9837d4293aac
GMTurbo <gabe.testa@gmail.com>
2015-04-28 11:22:33 -0600

    Update solution.hs
    added exercise 2

--- cis194/week2/solution.hs
+++ cis194/week2/solution.hs
@@ -2,54 +2,13 @@ module LogAnalysis where
 
 import Log
 
-
-checkFirst :: String -> Bool
-checkFirst letter = case letter of
-  "I" -> True
-  "W" -> True
-  "E" -> True
-  (_)  -> False
-
-getLogType :: String -> MessageType
-getLogType message =
-  let split = words message
-      first = head split
-  in case first of
-   "I" -> Info
-   "W" -> Warning
-   "E" -> Error (read (head (tail split)) :: Int)
-
--- parseRest :: String -> (Int, String)
--- parseRest str = let (t, rest) = lex str
---   in (read int :: Int, tail rest )
-
-parseRest :: String -> (Int, String)
-parseRest rest =
-  let split = words rest
-   in (read (head split) :: Int, unwords (tail split))
-
--- (Error 2) 562 "help help"
-parseMessage :: String -> LogMessage
-parseMessage message
-  | checkFirst(head (words message)) =
-       let logType = getLogType message
-           shrunk = tail(words message)
-           rest = parseRest(unwords shrunk)
-           timestamp = fst rest
-           last = snd rest
-         in case logType of
-           Error (x)-> let timestamp2 = (read (head (words last)) :: Int)
-                           last2 = unwords(tail (words last))
-                        in LogMessage logType timestamp2 last2
-           (_) -> LogMessage logType timestamp last
-  | otherwise = Unknown message
-
+-- Exercise 1
 parse :: String -> [LogMessage]
-parse str = map parseMessage2 entries
+parse str = map parseMessage entries
    where entries = (lines str) --filter (\s -> head (head s) =="E")
 
-parseMessage2 :: String -> LogMessage
-parseMessage2 str =
+parseMessage :: String -> LogMessage
+parseMessage str =
     let wordlist = words str in
      case wordlist of
       ("I":ts:msg) -> LogMessage Info (read ts :: Int) (unwords msg)
@@ -58,10 +17,10 @@ parseMessage2 str =
       (_) -> Unknown (unwords wordlist)
 
 -- Exercise 2
+insert :: LogMessage -> MessageTree -> MessageTree
+insert logMsg1@(LogMessage _ tsNew _) (Node left logMsg2@(LogMessage _ tsOld _ ) right)
+  | tsNew > tsOld = Node left logMsg1 (insert logMsg2 right)
+  | otherwise = Node (insert logMsg1 left) logMsg2 right
 
--- inserts a new LogMessage into an existing MessageTree, producing
--- a new MessageTree
--- insert :: LogMessage -> MessageTree -> MessageTree
--- insert log tree = case log of
---   Unknown -> tree
---   (_) -> let
+insert logMsg Leaf =
+  MessageTree Leaf logMsg Leaf
